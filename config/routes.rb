5e97d71f4c7672ef40b544e1f385abb7829977cc
Freddy Abelard <abelard.freddy@gmail.com>
2022-03-01 17:26:57 -0500

    modify the routes, it should be good

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :plants, only: [:index]
-  resources :user_plants, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :user_plants, only: [:create, :show, :edit, :update, :destroy]
   resources :user_tasks, only: [:index, :show, :create]
+
+  get 'my_plants', to: 'user_plants#index'
+  get 'add_info', to: 'pages#add_info'
 end
