04fbd23c27f35f884b99628e137395b3baf21e2c
lael aichaoui <lael.aichaoui@gmail.com>
2022-03-01 11:27:08 -0800

    env file generated and key added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Clean the DB
+User.destroy_all
+Plant.destroy_all
+Task.destroy_all
+
+User.create!(email: "test@test.com", password: "123456")
