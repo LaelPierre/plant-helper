280b54091ac4d1d9315134bd7210874acd9c338c
Freddy Abelard <abelard.freddy@gmail.com>
2022-03-02 16:23:18 -0500

    modal is ok

--- app/models/user_plant.rb
+++ app/models/user_plant.rb
@@ -2,4 +2,16 @@ class UserPlant < ApplicationRecord
   belongs_to :user
   belongs_to :plant
   has_many :user_tasks, dependent: :destroy
+
+  enum plant_age: {
+    young: 0,
+    advanced: 1,
+    old: 2
+  }
+
+  enum plant_health: {
+    bad: 0,
+    acceptable: 1,
+    good: 2
+  }
 end
