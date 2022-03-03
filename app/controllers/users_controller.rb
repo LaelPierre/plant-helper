7827b8d2031ee078be8f8a089f97ebe12a54d8e0
lael aichaoui <lael.aichaoui@gmail.com>
2022-03-03 11:52:47 -0800

    form added and can update plant info now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def update
+    current_user.update(user_params)
+    redirect_to my_plants_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:temperature, :sun_exposure, :prefered_day)
   end
 end
