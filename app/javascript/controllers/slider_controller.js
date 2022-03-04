d8d6cbba3fa9463f1ac184668ef1863f2087db50
Dan Amar <danamar2001@gmail.com>
2022-03-04 15:03:07 -0500

    roadblock while editting slider. Will resume monday

---
+++ app/javascript/controllers/slider_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "stimulus"
+import Slider from "bootstrap-slider"
+
+export default class extends Controller {
+  static targets = [ "tempSlider" ]
+
+  connect() {
+    // const slider = new Slider(this.tempSliderTarget.id, {
+    // const slider = new Slider("ex8", {
+    //   // tooltip: 'always'
+    // });
+    // console.log(slider)
+  }
+}
