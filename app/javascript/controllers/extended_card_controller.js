faf91278d1b62c9dbf274063a99c9d6c57f0571d
Freddy Abelard <abelard.freddy@gmail.com>
2022-03-07 14:50:14 -0500

    Extended card is done, need a little scroll fix

---
+++ app/javascript/controllers/extended_card_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["extendCard", "card"]
+
+  connect() {
+    // console.log(this.cardTarget)
+  }
+
+  revealCard() {
+    console.log(this.element);
+    this.extendCardTarget.classList.remove("d-none")
+    this.extendCardTarget.classList.add("d-flex")
+    this.extendCardTarget.classList.add("plant-info-popup")
+
+  }
+
+  hideCard() {
+    console.log(this.element);
+    this.extendCardTarget.classList.remove("d-flex")
+    this.extendCardTarget.classList.add("d-none")
+    this.extendCardTarget.classList.remove("plant-info-popup")
+  }
+}
